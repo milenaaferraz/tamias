d3d3d7a9cd17edcd3bd95b822d06087e986fafe7
Renata Porto <renatanogueirap@gmail.com>
2017-11-29 09:47:07 -0300

    segues ok boots ok primeira tela 100% referenciada

---
+++ tamias/BauViewController.swift
@@ -0,0 +1,43 @@
+//
+//  BauViewController.swift
+//  tamias
+//
+//  Created by Renata Nogueira Porto on 29/11/17.
+//  Copyright © 2017 Milena Almeida Ferraz. All rights reserved.
+//
+
+import UIKit
+
+class BauViewController: UIViewController {
+
+    @IBOutlet weak var metasOutlet: UIButton!
+    @IBOutlet weak var metasBotao: UIButton!
+    
+    @IBOutlet weak var jardimOutlet: UIButton!
+    @IBOutlet weak var jardimBotao: UIButton!
+    
+    @IBOutlet weak var bauOutlet: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
