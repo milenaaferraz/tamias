453de4f74ad96e290e10182091cf0f5bd1186095
Unknown <Flavia.n.godoy@gmail.com>
2017-11-29 11:01:45 -0300

    adicionei um comentario sobre milena ser um lixo
    milena eh um lixo mt a

--- tamias/RecompensaViewController.swift
+++ tamias/RecompensaViewController.swift
@@ -16,7 +16,7 @@ class RecompensaViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() { // oi
+    override func didReceiveMemoryWarning() { // tu eh um lixo
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
