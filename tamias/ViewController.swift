e88c2e9c86cfc4a68129b55192402d1dcb38b9b3
Unknown <renatanogueirap@gmail.com>
2017-11-29 14:41:22 -0300

    DEU BOM DEMAIS
    canto arredondado em 1 dia
    telas ligadas
    telas do exercicio 1

--- tamias/ViewController.swift
+++ tamias/ViewController.swift
@@ -15,43 +15,11 @@ class ViewController: UIViewController {
     // Jardim
     @IBOutlet weak var viewDoJardim: UIView!
     @IBOutlet weak var imagemDaCerca: UIImageView!
+    
     // Terras e plantas (linhas de 0 a 3 / colunas de 0 a 3 - para funcionar em um array)
     // Linha 0
-//    @IBOutlet weak var terra00: UIImageView!
-//    @IBOutlet weak var planta00: UIImageView!
-//    @IBOutlet weak var terra01: UIImageView!
-//    @IBOutlet weak var planta01: UIImageView!
-//    @IBOutlet weak var terra02: UIImageView!
-//    @IBOutlet weak var planta02: UIImageView!
-//    @IBOutlet weak var terra03: UIImageView!
-//    @IBOutlet weak var planta03: UIImageView!
-//    // Linha 1
-//    @IBOutlet weak var terra10: UIImageView!
-//    @IBOutlet weak var planta10: UIImageView!
-//    @IBOutlet weak var terra11: UIImageView!
-//    @IBOutlet weak var planta11: UIImageView!
-//    @IBOutlet weak var terra12: UIImageView!
-//    @IBOutlet weak var planta12: UIImageView!
-//    @IBOutlet weak var terra13: UIImageView!
-//    @IBOutlet weak var planta13: UIImageView!
-//    //Linha 2
-//    @IBOutlet weak var terra20: UIImageView!
-//    @IBOutlet weak var planta20: UIImageView!
-//    @IBOutlet weak var terra21: UIImageView!
-//    @IBOutlet weak var planta21: UIImageView!
-//    @IBOutlet weak var terra22: UIImageView!
-//    @IBOutlet weak var planta22: UIImageView!
-//    @IBOutlet weak var terra23: UIImageView!
-//    @IBOutlet weak var planta23: UIImageView!
-//    // Linha 3
-//    @IBOutlet weak var terra30: UIImageView!
-//    @IBOutlet weak var planta30: UIImageView!
-//    @IBOutlet weak var terra31: UIImageView!
-//    @IBOutlet weak var planta31: UIImageView!
-//    @IBOutlet weak var terra32: UIImageView!
-//    @IBOutlet weak var planta32: UIImageView!
-//    @IBOutlet weak var terra33: UIImageView!
-//    @IBOutlet weak var planta33: UIImageView!
+    
+
     // Conjunto de terras
     
     // Solzinhos
