ded90749ec4bbb0247d0ce2431c8cdeb849a006e
milena <milenaaferraz99@gmail.com>
2017-12-04 15:16:50 -0300

    Auto stash before merge of "master" and "origin/master"

--- tamias/Exercicio2ViewController.swift
+++ tamias/Exercicio2ViewController.swift
@@ -77,6 +77,11 @@ class Exercicio2ViewController:UIViewController {
             }
         }
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -114,14 +119,5 @@ class Exercicio2ViewController:UIViewController {
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
