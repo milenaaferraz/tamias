b4642e70b797a8ee8ecefb48302feecabc700707
milena <milenaaferraz99@gmail.com>
2017-12-04 11:12:41 -0300

    agora o genius vaiiii

--- tamias/Exercicio2ViewController.swift
+++ tamias/Exercicio2ViewController.swift
@@ -8,28 +8,21 @@
 
 import UIKit
 
-class Exercicio2ViewController:
-    
-    UIViewController {
-    @IBOutlet weak var imagem4: UIImageView!
-    @IBOutlet weak var imagem3: UIImageView!
-    @IBOutlet weak var imagem1: UIImageView!
-    @IBOutlet weak var imagem2: UIImageView!
+class Exercicio2ViewController:UIViewController {
+
+    @IBOutlet weak var flor4: UIButton!
+    @IBOutlet weak var flor3: UIButton!
+    @IBOutlet weak var flor2: UIButton!
+    @IBOutlet weak var flor1: UIButton!
     @IBOutlet weak var numeroPontos: UILabel!
     @IBOutlet weak var suaPontuacao: UILabel!
     @IBOutlet weak var instrucaoNoJogo: UILabel!
     @IBAction func botaox(_ sender: Any) {
     }
+    var clickUsuario: [Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imagem1.isHidden = true
-        imagem2.isHidden = true
-        imagem3.isHidden = true
-        imagem4.isHidden = true
-        
-        
-       
         // Do any additional setup after loading the view.
     }
 
@@ -38,7 +31,22 @@ class Exercicio2ViewController:
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func click(_ sender: Any) {
+        if sender as! UIButton == flor1{
+            clickUsuario.append(1)
+            print(1)
+        } else if sender as! UIButton == flor2{
+            clickUsuario.append(2)
+            print(2)
+        } else if sender as! UIButton == flor3 {
+            clickUsuario.append(3)
+            print(3)
+        } else if sender as! UIButton == flor4 {
+            clickUsuario.append(4)
+            print (4)
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
