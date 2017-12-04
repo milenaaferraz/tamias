52560819c098d7aafa082d80a77dabc6ee96bd65
milena <milenaaferraz99@gmail.com>
2017-12-04 13:30:12 -0300

    aiai

--- tamias/Exercicio2ViewController.swift
+++ tamias/Exercicio2ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class Exercicio2ViewController:UIViewController {
 
+    var timer: Timer? = nil
+    var indice: Int = 0
     @IBOutlet weak var flor4: UIButton!
     @IBOutlet weak var flor3: UIButton!
     @IBOutlet weak var flor2: UIButton!
@@ -20,15 +22,45 @@ class Exercicio2ViewController:UIViewController {
     @IBAction func botaox(_ sender: Any) {
     }
     var clickUsuario: [Int] = []
+    var sequenciaQueAparece: [Int] = []
+    let listaDeSequencias: [[Int]] = [[1,2,3,4], [1,2,4,3], [1,3,2,4], [1,3,4,2], [1,4,3,2], [1,4,2,3], [2,1,3,4], [2,1,4,3], [2,3,1,4], [2,3,4,1], [2,4,1,3], [2,4,3,1], [3,1,2,4], [3,1,4,2], [3,2,1,4], [3,2,4,1], [3,4,1,2], [3,4,2,1], [4,1,2,3], [4,1,3,2], [4,2,1,3], [4,2,3,1], [4,3,1,2], [4,3,2,1]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        flor1.isHidden = true
+        flor2.isHidden = true
+        flor3.isHidden = true
+        flor4.isHidden = true
+        
+        //DETERMINA A SEQUENCIA ALEATÓRIA DAS FLORES
+        let numero = arc4random_uniform(UInt32(listaDeSequencias.count))
+        print(numero)
+        sequenciaQueAparece = listaDeSequencias[Int(numero)]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+          timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            if self.indice == 4{
+                timer.invalidate()
+            }else{
+                let numero = self.sequenciaQueAparece[self.indice]
+                
+                if numero == 1{
+                    self.flor1.isHidden = false
+                }else if numero == 2{
+                    self.flor2.isHidden = false
+                }else if numero == 3{
+                    self.flor3.isHidden = false
+                }else if numero == 4{
+                    self.flor4.isHidden = false
+                }
+                
+                self.indice = self.indice + 1
+            }
+        }
     }
     
     @IBAction func click(_ sender: Any) {
