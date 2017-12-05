b30e0403d0660080b289101c39908e7bb846a75a
milena <milenaaferraz99@gmail.com>
2017-12-05 18:23:58 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ tamias/Exercicio2RecompensaViewController.swift
@@ -0,0 +1,37 @@
+//
+//  Exercicio2RecompensaViewController.swift
+//  tamias
+//
+//  Created by Milena Almeida Ferraz on 05/12/17.
+//  Copyright © 2017 Milena Almeida Ferraz. All rights reserved.
+//
+
+import UIKit
+
+class Exercicio2RecompensaViewController: UIViewController {
+
+    var vidas2:Int = 0
+    var pontucao2:Int = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
