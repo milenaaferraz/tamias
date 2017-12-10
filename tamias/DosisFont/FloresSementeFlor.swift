28ed171f622273a2a2ef6242fc1c1d3d03c42227
Unknown <Flavia.n.godoy@gmail.com>
2017-12-10 11:12:24 -0300

    ajeitei a dosis daquela tela e criei uma classe q vou ver se vai servir
    pra alguma coisa

---
+++ tamias/DosisFont/FloresSementeFlor.swift
@@ -0,0 +1,21 @@
+//
+//  FloresSementeFlor.swift
+//  tamias
+//
+//  Created by Ana Flávia Nascimento Godoy on 12/10/17.
+//  Copyright © 2017 Milena Almeida Ferraz. All rights reserved.
+//
+
+import Foundation
+
+
+class FloresSementeFlor {
+    
+    var sementeDeSementes:[String]
+    var florPraPlantar:[String]
+    
+    init (sementeDeSementes:[String], florPraPlantar:[String]) {
+        self.sementeDeSementes = sementeDeSementes
+        self.florPraPlantar = florPraPlantar
+    }
+}
