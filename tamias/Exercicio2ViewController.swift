9222496d7e46556e3733f349e8760a98b36c682e
Unknown <renatanogueirap@gmail.com>
2017-12-20 11:10:10 -0300

    tive que descartar o autolayout do jardim :(
    mas agora organizei algumas coisas e ajeitei alguns bugs

--- tamias/Exercicio2ViewController.swift
+++ tamias/Exercicio2ViewController.swift
@@ -24,7 +24,7 @@ class Exercicio2ViewController:UIViewController {
         jogoAparece()
         acertouErrou.isHidden = true
         proxSeqOutlet.isHidden = true
-        suaPontuacao.isHidden = false
+        suaPontuacao.isHidden = true
     }
     
     @IBOutlet weak var proxSeqOutlet: UIButton!
